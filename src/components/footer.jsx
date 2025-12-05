82a499e5f018248ab3fc0bf292a5280749ce3a33
Yeysonacosta <deyviacosta900@gmail.com>
2025-12-04 20:58:39 -0500

    ADD: se creó el componente Footer con contenido del pie de página

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© {new Date().getFullYear()} Sistema de Control Académico - UCV</p>
+        <p>Desarrollado por cada uno de los miembros del proyecto </p>
+      </div>
+    </footer>
+  );
+}
